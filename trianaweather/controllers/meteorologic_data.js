16752be025415bff178e865624325f01403e7254
Daniel Santano Fernández <dsantano@AULAS.TRIANA>
2019-12-17 14:29:59 +0100

    mas cambios 17/12

--- trianaweather/controllers/meteorologic_data.js
+++ trianaweather/controllers/meteorologic_data.js
@@ -6,17 +6,6 @@ const Station = require('../models/meteorologic_station');
 
 module.exports = {
     nuevaData: (req, res) => {
-        let maitenanced_user;
-        User.findOne({"stations_maitenancing": req.body.station}, (err, user) => {
-            if(err) res.send(500, err.message);
-            maitenanced_user = user;
-        });
-
-        let station_of_data;
-        Station.findOne({"_id": req.body.station}, (err, station) => {
-            if(err) res.send(500, err.message);
-            station_of_data = station;
-        });
 
         let meteorologicData = new MeteorologicData({
             latitude: req.body.latitude,
@@ -24,13 +13,16 @@ module.exports = {
             name: req.body.name,
             //registed_at: Date.now,
             registed_by: req.user,
-            maitenanced_by: maitenanced_user,
-            station: station_of_data
+            maitenanced_by: req.body.maitenanced_by,
+            station: req.body.station
         });
-        meteorologicData.save((err, meteorologicData) => {
-            if(err) res.send(500, err.message);
-            res.status(201).json(meteorologicData);
-        });
+        meteorologicData.save()
+            .then(d => {console.log(d)
+                d.populate('registed_by', {fullname: 1, email: 1}).execPopulate()})
+            .then(d => d.populate('maitenanced_by', {fullname: 1, email: 1}).execPopulate())
+            .then(d => d.populate('station').execPopulate())
+            .then(d => res.status(200).json(d))
+            .catch( err => res.send(500, err.message));
     },
     getDataById: (req, res) => {
         /* MeteorologicData.findOne({"_id": req.params.id}, (err, data) => {
@@ -47,11 +39,12 @@ module.exports = {
             });
         }); */
         MeteorologicData.findOne({"_id": req.params.id})
-            .then(d => d.populate('registed_by', {fullname: 1, email: 1}).execPopulate())
+            .then(d => {console.log(d)
+                d.populate('registed_by', {fullname: 1, email: 1}).execPopulate()})
             .then(d => d.populate('maitenanced_by', {fullname: 1, email: 1}).execPopulate())
             .then(d => d.populate('station').execPopulate())
             .then(d => res.status(200).json(d))
-            .catch( err => res.send(500, err.message));
+            .catch( err => res.status(500).send(err.message));
     },
     getDataByStationId: (req, res) => {
         MeteorologicData.find({"station": req.params.id}, (err, data) => {
--- trianaweather/routes/meteorologic_data.js
+++ trianaweather/routes/meteorologic_data.js
@@ -1,5 +1,5 @@
 'use strict'
-/*
+
 const express = require('express')
 const router = express.Router()
 
@@ -7,10 +7,10 @@ const authMiddleware = require('../middleware/auth');
 const roleManagerMiddleware = require('../middleware/has_role_manager');
 const mDataController = require('../controllers/meteorologic_data');
 
-router.post('/weather', authMiddleware, roleManagerMiddleware, mDataController.nuevaData);
-router.get('/weather/:id', authMiddleware, mDataController.getDataById);
-router.get('/stations/:id/weather/', authMiddleware, mDataController.getDataByStationId);
-router.get('/stations/:id/weather/from/:from/to/:to', authMiddleware, mDataController.getDataByStationId);
+router.post('/weather',  mDataController.nuevaData);
+router.get('/weather/:id',  mDataController.getDataById);
+// authMiddleware.ensureAuthenticated, authMiddleware.ensureAuthenticated, roleManagerMiddleware.ensureRoleManager,
+router.get('/stations/:id/weather/', authMiddleware.ensureAuthenticated, mDataController.getDataByStationId);
+router.get('/stations/:id/weather/from/:from/to/:to', authMiddleware.ensureAuthenticated, mDataController.getDataByStationId);
 
 module.exports = router
-*/
