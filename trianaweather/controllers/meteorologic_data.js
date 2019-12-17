c5784112e882f4f14efd4818a95851743203eac9
Daniel Santano Fernández <danielsantanof99@gmail.com>
2019-12-17 19:56:02 +0100

    no funcionan los populate

--- trianaweather/controllers/meteorologic_data.js
+++ trianaweather/controllers/meteorologic_data.js
@@ -5,28 +5,40 @@ const User = require('../models/user');
 const Station = require('../models/meteorologic_station');
 
 module.exports = {
-    nuevaData: (req, res) => {
-
+    nuevaData: async (req, res) => {
         let meteorologicData = new MeteorologicData({
             latitude: req.body.latitude,
             longitude: req.body.longitude,
             name: req.body.name,
-            //registed_at: Date.now,
             registed_by: req.user,
             maitenanced_by: req.body.maitenanced_by,
             station: req.body.station
         });
-        meteorologicData.save()
-            .then(d => {console.log(d)
-                d.populate('registed_by', {fullname: 1, email: 1}).execPopulate()})
+        await meteorologicData.save()
+            .then(d => d.populate('registed_by', {fullname: 1, email: 1}).execPopulate())
             .then(d => d.populate('maitenanced_by', {fullname: 1, email: 1}).execPopulate())
             .then(d => d.populate('station').execPopulate())
             .then(d => res.status(200).json(d))
             .catch( err => res.send(500, err.message));
+        /* await meteorologicData.save()
+        .then(data => {
+            User.populate(data, {path: "registed_by"},(err, data) =>{
+                if(err) res.send(500, err.message);
+                User.populate(data, {path: "maitenanced_by"},(err, data) =>{
+                    if(err) res.send(500, err.message);
+                    Station.populate(data, {path: "station"},(err, data) =>{
+                        if(err) res.send(500, err.message);
+                        res.status(200).json(data);
+                    });
+                });
+            });
+        })
+        .catch(err => res.send(500, err.message)); */
     },
     getDataById: (req, res) => {
-        /* MeteorologicData.findOne({"_id": req.params.id}, (err, data) => {
+        /* MeteorologicData.findOne({_id: req.params.id}, (err, data) => {
             if(err) res.send(500, err.message);
+            console.log(data)
             User.populate(data, {path: "registed_by"},(err, data) =>{
                 if(err) res.send(500, err.message);
                 User.populate(data, {path: "maitenanced_by"},(err, data) =>{
@@ -38,13 +50,36 @@ module.exports = {
                 });
             });
         }); */
-        MeteorologicData.findOne({"_id": req.params.id})
+        /* MeteorologicData.findOne({_id: req.params.id}).populate([{
+            path: 'registed_by',
+            model: 'User'
+        }, {
+            path: 'maitenanced_by',
+            model: 'User'
+        }, {
+            path: 'station',
+            model: 'MeteorologicStation'
+        }]).exec((err, data) => {
+            if(err) res.status(500).send(err.message);
+            res.status(200).json(data);
+        }) */
+        MeteorologicData.findOne({_id: req.params.id})
+            .populate({ path: 'registed_by', select: 'fullname' })
+            .populate({ path: 'registed_by', select: 'email' })
+            .populate({ path: 'maitenanced_by', select: 'fullname' })
+            .populate({ path: 'maitenanced_by', select: 'email' })
+            .populate('station')
+            .exec((err, data) => {
+                if(err) res.status(500).send(err.message);
+                res.status(200).json(data);
+            });
+        /* MeteorologicData.findOne({_id: req.params.id})
             .then(d => {console.log(d)
                 d.populate('registed_by', {fullname: 1, email: 1}).execPopulate()})
             .then(d => d.populate('maitenanced_by', {fullname: 1, email: 1}).execPopulate())
             .then(d => d.populate('station').execPopulate())
             .then(d => res.status(200).json(d))
-            .catch( err => res.status(500).send(err.message));
+            .catch( err => res.status(500).send(err.message)); */
     },
     getDataByStationId: (req, res) => {
         MeteorologicData.find({"station": req.params.id}, (err, data) => {
