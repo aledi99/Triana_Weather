284ea1564b7aeb36c0c7981f1cc90540877ec041
Daniel Santano Fernández <dsantano@AULAS.TRIANA>
2019-12-18 08:53:03 +0100

    metereologic_data controllers funcionan parte dsantano

--- trianaweather/controllers/meteorologic_data.js
+++ trianaweather/controllers/meteorologic_data.js
@@ -10,138 +10,49 @@ module.exports = {
             latitude: req.body.latitude,
             longitude: req.body.longitude,
             name: req.body.name,
-            registed_by: req.user,
+            registed_by: req.body.registed_by,
             maitenanced_by: req.body.maitenanced_by,
             station: req.body.station
         });
-        /* await meteorologicData.save()
+        await meteorologicData.save()
             .then(d => d.populate('registed_by', {fullname: 1, email: 1}).execPopulate())
             .then(d => d.populate('maitenanced_by', {fullname: 1, email: 1}).execPopulate())
             .then(d => d.populate('station').execPopulate())
             .then(d => res.status(200).json(d))
-            .catch( err => res.send(500, err.message)); */
-
-            //ERROR: utils.populate: invalid path. Expected string. Got typeof `object`
-        await meteorologicData.save().then(data => {
-            data.populate({
-                path: 'station',
-                model: 'MeteorologicStation',
-                populate: {
-                    parth: 'registed_by maitenanced_by',
-                    model: 'User',
-                    select: 'fullname email'
-                }
-            }).exec((err, resp) => {
-                if(err) res.status(500).send(err.message);
-                res.status(201).json(data);
-            });
-        }).catch(err => res.status(500).send(err.message));
-        
-        /* await meteorologicData.save()
-        .then(data => {
-            User.populate(data, {path: "registed_by"},(err, data) =>{
-                if(err) res.send(500, err.message);
-                User.populate(data, {path: "maitenanced_by"},(err, data) =>{
-                    if(err) res.send(500, err.message);
-                    Station.populate(data, {path: "station"},(err, data) =>{
-                        if(err) res.send(500, err.message);
-                        res.status(200).json(data);
-                    });
-                });
-            });
-        })
-        .catch(err => res.send(500, err.message)); */
+            .catch( err => res.send(500, err.message));
     },
     getDataById: (req, res) => {
-        MeteorologicData.findOne({_id: req.params.id}).populate({
-            path: 'station',
-            model: 'MeteorologicStation',
-            populate: {
-                parth: 'registed_by maitenanced_by',
-                model: 'User',
-                select: 'fullname email'
-            }
-        }).exec((err, data) => {
-            if(err) res.status(500).send(err.message);
-            res.status(200).json(data);
-        });
-        /* MeteorologicData.findOne({_id: req.params.id}, (err, data) => {
-            if(err) res.send(500, err.message);
-            console.log(data)
-            User.populate(data, {path: "registed_by"},(err, data) =>{
-                if(err) res.send(500, err.message);
-                User.populate(data, {path: "maitenanced_by"},(err, data) =>{
-                    if(err) res.send(500, err.message);
-                    Station.populate(data, {path: "station"},(err, data) =>{
-                        if(err) res.send(500, err.message);
-                        res.status(200).json(data);
-                    });
-                });
-            });
-        }); */
-        /* MeteorologicData.findOne({_id: req.params.id}).populate([{
-            path: 'registed_by',
-            model: 'User'
-        }, {
-            path: 'maitenanced_by',
-            model: 'User'
-        }, {
-            path: 'station',
-            model: 'MeteorologicStation'
-        }]).exec((err, data) => {
-            if(err) res.status(500).send(err.message);
-            res.status(200).json(data);
-        }) */
-        /* MeteorologicData.findOne({_id: req.params.id})
-            .populate({ path: 'registed_by', select: 'fullname' })
-            .populate({ path: 'registed_by', select: 'email' })
-            .populate({ path: 'maitenanced_by', select: 'fullname' })
-            .populate({ path: 'maitenanced_by', select: 'email' })
-            .populate('station')
-            .exec((err, data) => {
-                if(err) res.status(500).send(err.message);
-                res.status(200).json(data);
-            }); */
-        /* MeteorologicData.findOne({_id: req.params.id})
-            .then(d => {console.log(d)
-                d.populate('registed_by', {fullname: 1, email: 1}).execPopulate()})
+        MeteorologicData.findById(req.params.id)
+            .exec()
+            .then(d => d.populate('registed_by', {fullname: 1, email: 1}).execPopulate())
             .then(d => d.populate('maitenanced_by', {fullname: 1, email: 1}).execPopulate())
             .then(d => d.populate('station').execPopulate())
             .then(d => res.status(200).json(d))
-            .catch( err => res.status(500).send(err.message)); */
+            .catch( err => res.status(500).send(err.message));
     },
     getDataByStationId: (req, res) => {
-        MeteorologicData.find({"station": req.params.id}, (err, data) => {
-            if(err) res.send(500, err.message);
-            User.populate(data, {path: "registed_by"},(err, data) =>{
-                if(err) res.send(500, err.message);
-                User.populate(data, {path: "maitenanced_by"},(err, data) =>{
-                    if(err) res.send(500, err.message);
-                    Station.populate(data, {path: "station"},(err, data) =>{
-                        if(err) res.send(500, err.message);
-                        res.status(200).json(data);
-                    });
-                });
-            });
-        });
+        MeteorologicData.find({"station": req.params.id})
+            .populate('registed_by', {fullname: 1, email: 1})
+            .populate('maitenanced_by', {fullname: 1, email: 1})
+            .populate('station')
+            .exec()
+            .then(d => res.status(200).json(d))
+            .catch( err => res.status(500).send(err.message));
     },
     getDataFromStationFromDate1ToDate2: (req, res) => {
         MeteorologicData.find({"station": req.params.id, "registed_at": {
             $gte: req.params.from,
             $lt: req.params.to
-        }}, (err, data) => {
-                if(err) res.send(500, err.message);
-                User.populate(data, {path: "registed_by"},(err, data) =>{
-                    if(err) res.send(500, err.message);
-                    User.populate(data, {path: "maitenanced_by"},(err, data) =>{
-                        if(err) res.send(500, err.message);
-                        Station.populate(data, {path: "station"},(err, data) =>{
-                            if(err) res.send(500, err.message);
-                            res.status(200).json(data);
-                        });
-                    });
-                });
-            });
+            }})
+            .populate('registed_by', {fullname: 1, email: 1})
+            .populate('maitenanced_by', {fullname: 1, email: 1})
+            .populate('station')
+            .exec()
+            .then(d => res.status(200).json(d))
+            .catch( err => res.status(500).send(err.message));
+    },
+    getDataFromToday: (req, res) => {
+        //TODO ALEJANDRO
     }
 
 }
--- trianaweather/routes/meteorologic_data.js
+++ trianaweather/routes/meteorologic_data.js
@@ -7,10 +7,10 @@ const authMiddleware = require('../middleware/auth');
 const roleManagerMiddleware = require('../middleware/has_role_manager');
 const mDataController = require('../controllers/meteorologic_data');
 
-router.post('/weather',  mDataController.nuevaData);
-router.get('/weather/:id',  mDataController.getDataById);
-// authMiddleware.ensureAuthenticated, authMiddleware.ensureAuthenticated, roleManagerMiddleware.ensureRoleManager,
+router.post('/weather', authMiddleware.ensureAuthenticated, roleManagerMiddleware.ensureRoleManager, mDataController.nuevaData);
+router.get('/weather/:id', authMiddleware.ensureAuthenticated, mDataController.getDataById);
 router.get('/stations/:id/weather/', authMiddleware.ensureAuthenticated, mDataController.getDataByStationId);
 router.get('/stations/:id/weather/from/:from/to/:to', authMiddleware.ensureAuthenticated, mDataController.getDataByStationId);
+router.get('/weather/today', authMiddleware.ensureAuthenticated, mDataController.getDataFromToday);
 
 module.exports = router
