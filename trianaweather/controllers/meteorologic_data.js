67b09a8fe94cdded3b0f0af83c5ccb04cda83df8
Daniel Santano Fernández <danielsantanof99@gmail.com>
2019-12-18 02:15:58 +0100

    mas errores

--- trianaweather/controllers/meteorologic_data.js
+++ trianaweather/controllers/meteorologic_data.js
@@ -14,12 +14,29 @@ module.exports = {
             maitenanced_by: req.body.maitenanced_by,
             station: req.body.station
         });
-        await meteorologicData.save()
+        /* await meteorologicData.save()
             .then(d => d.populate('registed_by', {fullname: 1, email: 1}).execPopulate())
             .then(d => d.populate('maitenanced_by', {fullname: 1, email: 1}).execPopulate())
             .then(d => d.populate('station').execPopulate())
             .then(d => res.status(200).json(d))
-            .catch( err => res.send(500, err.message));
+            .catch( err => res.send(500, err.message)); */
+
+            //ERROR: utils.populate: invalid path. Expected string. Got typeof `object`
+        await meteorologicData.save().then(data => {
+            data.populate({
+                path: 'station',
+                model: 'MeteorologicStation',
+                populate: {
+                    parth: 'registed_by maitenanced_by',
+                    model: 'User',
+                    select: 'fullname email'
+                }
+            }).exec((err, resp) => {
+                if(err) res.status(500).send(err.message);
+                res.status(201).json(data);
+            });
+        }).catch(err => res.status(500).send(err.message));
+        
         /* await meteorologicData.save()
         .then(data => {
             User.populate(data, {path: "registed_by"},(err, data) =>{
@@ -36,6 +53,18 @@ module.exports = {
         .catch(err => res.send(500, err.message)); */
     },
     getDataById: (req, res) => {
+        MeteorologicData.findOne({_id: req.params.id}).populate({
+            path: 'station',
+            model: 'MeteorologicStation',
+            populate: {
+                parth: 'registed_by maitenanced_by',
+                model: 'User',
+                select: 'fullname email'
+            }
+        }).exec((err, data) => {
+            if(err) res.status(500).send(err.message);
+            res.status(200).json(data);
+        });
         /* MeteorologicData.findOne({_id: req.params.id}, (err, data) => {
             if(err) res.send(500, err.message);
             console.log(data)
@@ -63,7 +92,7 @@ module.exports = {
             if(err) res.status(500).send(err.message);
             res.status(200).json(data);
         }) */
-        MeteorologicData.findOne({_id: req.params.id})
+        /* MeteorologicData.findOne({_id: req.params.id})
             .populate({ path: 'registed_by', select: 'fullname' })
             .populate({ path: 'registed_by', select: 'email' })
             .populate({ path: 'maitenanced_by', select: 'fullname' })
@@ -72,7 +101,7 @@ module.exports = {
             .exec((err, data) => {
                 if(err) res.status(500).send(err.message);
                 res.status(200).json(data);
-            });
+            }); */
         /* MeteorologicData.findOne({_id: req.params.id})
             .then(d => {console.log(d)
                 d.populate('registed_by', {fullname: 1, email: 1}).execPopulate()})
