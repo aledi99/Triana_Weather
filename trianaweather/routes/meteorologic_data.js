47901a10b5bad94a673af78ce4461b8a92ad598d
Alejandro Díaz Santos <alediazuni@gmail.com>
2019-12-19 13:28:44 +0000

    Ruta

--- trianaweather/routes/meteorologic_data.js
+++ trianaweather/routes/meteorologic_data.js
@@ -7,6 +7,7 @@ const authMiddleware = require('../middleware/auth');
 const roleManagerMiddleware = require('../middleware/has_role_manager');
 const mDataController = require('../controllers/meteorologic_data');
 
+router.get('/stations/:id/summary/today', authMiddleware.ensureAuthenticated, mDataController.getSummary);
 router.get('/weather/today', authMiddleware.ensureAuthenticated, mDataController.getDataFromToday);
 router.post('/weather', authMiddleware.ensureAuthenticated, roleManagerMiddleware.ensureRoleManager, mDataController.nuevaData);
 router.get('/weather/:id', authMiddleware.ensureAuthenticated, mDataController.getDataById);
